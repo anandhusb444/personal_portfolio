d7e0478597928a741c74972149c5dfa9c41a9045
anandhusb444 <anandhusb444@gmail.com>
2024-06-02 11:01:28 +0530

    third commit

--- portfolio.js
+++
@@ -1,8 +0,0 @@
-// function sendMail(){
-//     let param = {
-//         name:document.getElementById("name").value,
-//         email:document.getElementById("email").value,
-//         message:document.getElementById("message").value,
-//     }
-//     emailjs.send("service_ehh6dgn","template_qhu55ew",param).then(alert("mail send successfully"))
-// }
